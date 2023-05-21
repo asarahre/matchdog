33aa1a1108e34af0655fa7414d619d4fec6509ff
asarahre <sarahbraga3433@gmail.com>
2023-05-21 00:31:26 -0400

    add style user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-
-<body>
+<x-layout title="Users">
     <p>Dados do user</p>
     @foreach ($user as $us)
     <ul>
@@ -17,6 +7,4 @@
         <li>Senha: {{ $us -> password }}</li>
     </ul>
     @endforeach
-</body>
-
-</html>
+</x-layout>
